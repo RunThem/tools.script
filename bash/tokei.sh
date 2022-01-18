0a5fa18cbcd760bd68100c666fe4056dc7823d70
RunThem <iccy.fun@outlook.com>
2022-01-18 11:28:29 +0800

    format the output of the `tokei` to avoid unnecessary data display

---
+++ bash/tokei.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2020 RunThem <iccy.fun@outlook.com>
+# All rights reserved. See MIT LICENSE for redistribution.
+
+IFS='
+'
+
+time=$(date +%N)
+buf=$(tokei --sort code .)
+
+printf "Tokei T(%.2fs)\n" $(echo "($(date +%N) - ${time}) * 0.000000001" | bc)
+
+for line in ${buf}
+do
+  echo ${line::53}
+done
+
+#!/usr/bin/env bash
+
+# IFS=$'\n'; start_time=$(date +%N); buf=$(tokei --sort code .); printf "Tokei T(%.2fs)\n" $(echo "($(date +%N)-${start_time})*0.000000001" | bc); for line in ${buf}; do echo "${line::53}"; done
