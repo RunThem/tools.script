8895370420ee5057ab4754982f8db40772ff9d11
RunThem <iccy.fun@outlook.com>
2022-02-20 23:34:04 +0800

    added a script to print CPU information

---
+++ bash/cpu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2020 RunThem <iccy.fun@outlook.com>
+# All rights reserved. See MIT LICENSE for redistribution.
+
+while [[ 1 ]]; do
+  clear && sensors && sleep 1
+done
